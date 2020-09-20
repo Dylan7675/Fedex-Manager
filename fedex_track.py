2f140374ac7fd0c4dcc1f261d38ccea8b6d14652
Dylan Martin <dylan.martin7675@gmail.com>
2020-09-20 14:19:29 -0700

    Changed tracking function to accept an individual tracking number instead of a list of tracking numbers.

--- fedex_track.py
+++ fedex_track.py
@@ -1,57 +1,46 @@
 import requests
 import json
-
+import sys
 
 class Tracker:
-    """
-    Tracker instanciates with a list of tracking numbers.
-    """
 
-    def __init__(self, tracking_list = []):
-        self.tracking_list = tracking_list
-
-    def track(self):
+    def track(self, tracking_number):
         """
-        Input: tracking_list of Tracker instance
-        Output: Returns a list of tracking statuses in order of input
+        Input: Tracking Number
+        Output: Returns tracking status
         """
 
-        tracked_list = []
+        # Requests data from fedex per tracking number
+        info = requests.post('https://www.fedex.com/trackingCal/track',
+        data={
+        'data': json.dumps({
+            'TrackPackagesRequest': {
+                'appType': 'wtrk',
+                'uniqueKey': '',
+                'processingParameters': {
+                    'anonymousTransaction': True,
+                    'clientId': 'WTRK',
+                    'returnDetailedErrors': True,
+                    'returnLocalizedDateTime': False
+                },
+                'trackingInfoList': [{
+                    'trackNumberInfo': {
+                        'trackingNumber': str(tracking_number),
+                        'trackingQualifier': '',
+                        'trackingCarrier': ''
+                    }
+                }]
+            }
+        }),
+        'action': 'trackpackages',
+        'locale': 'en_US',
+        'format': 'json',
+        'version': 99
+        }).json()
 
-        for tracking_number in self.tracking_list:
+        status = info['TrackPackagesResponse']['packageList'][0]['keyStatus']
 
-            info = requests.post('https://www.fedex.com/trackingCal/track',
-            data={
-            'data': json.dumps({
-                'TrackPackagesRequest': {
-                    'appType': 'wtrk',
-                    'uniqueKey': '',
-                    'processingParameters': {
-                        'anonymousTransaction': True,
-                        'clientId': 'WTRK',
-                        'returnDetailedErrors': True,
-                        'returnLocalizedDateTime': False
-                    },
-                    'trackingInfoList': [{
-                        'trackNumberInfo': {
-                            'trackingNumber': str(tracking_number),
-                            'trackingQualifier': '',
-                            'trackingCarrier': ''
-                        }
-                    }]
-                }
-            }),
-            'action': 'trackpackages',
-            'locale': 'en_US',
-            'format': 'json',
-            'version': 99
-            }).json()
-
-            status = info['TrackPackagesResponse']['packageList'][0]['keyStatus']
-            tracked_list.append(status)
-
-        return tracked_list
-
+        return status
 
 if __name__ == '__main__':
     main()
